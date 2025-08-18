212d636277bd9ddcbcfed89046b568ee643f7897
Ahmad Daud <ahmaddaud123@icloud.com>
2025-08-18 16:18:08 +0500

    ditch opengl, embrace macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,117 @@
-use beryllium::*;
+use ::glam::Vec2;
+use macroquad::prelude::*;
 
-fn main() {
-    let sdl = Sdl::init(init::InitFlags::EVERYTHING);
+// window size
+const WIDTH: i32 = 800;
+const HEIGHT: i32 = 600;
 
-    sdl.set_gl_context_major_version(3).unwrap();
-    sdl.set_gl_profile(video::GlProfile::Core).unwrap();
-    #[cfg(target_os = "macos")]
-    {
-        sdl.set_gl_context_flags(video::GlContextFlags::FORWARD_COMPATIBLE)
-            .unwrap();
+// scientific constants
+const G: f32 = 6.6743e-11;
+const SPEED_OF_LIGHT: f32 = 299792458.0;
+const SLOWDOWN: f32 = 1000000000.;
+
+// window configuration
+fn window_config() -> Conf {
+    Conf {
+        window_title: String::from("Gargantua Dev Build"),
+        window_width: WIDTH,
+        window_height: HEIGHT,
+        window_resizable: false,
+        ..Default::default()
+    }
+}
+
+//struct for Black Hole
+struct BlackHole {
+    position: Vec2,
+    mass: f32,
+    r_s: f32,
+}
+
+impl BlackHole {
+    fn calc_r_s(&mut self) {
+        self.r_s = (2.0 * G * self.mass) / (SPEED_OF_LIGHT * SPEED_OF_LIGHT);
     }
 
-    let win_args = video::CreateWinArgs {
-        title: "Gargantua Dev Build",
-        width: 800,
-        height: 600,
-        allow_high_dpi: true,
-        borderless: false,
-        resizable: false,
+    fn draw(&self) {
+        draw_circle(self.position.x, self.position.y, self.r_s, RED);
+    }
+}
+
+// struct for Ray
+struct Ray {
+    x: f32,
+    y: f32,
+    direction: Vec2,
+    trail: Vec<Vec2>,
+}
+
+impl Ray {
+    fn draw(&self) {
+        draw_rectangle(self.x, self.y, 10., 10., WHITE);
+
+        for i in 1..self.trail.len() {
+            println!("{}", (i / self.trail.len()));
+            draw_rectangle(
+                self.trail[i].x,
+                self.trail[i].y,
+                5.,
+                5.,
+                Color {
+                    r: 255.,
+                    g: 255.,
+                    b: 255.,
+                    a: (i / self.trail.len()) as f32,
+                },
+            );
+        }
+    }
+
+    fn push_back(&mut self) {
+        self.trail.push(Vec2 {
+            x: self.x,
+            y: self.y,
+        })
+    }
+
+    fn step(&mut self) {
+        self.x += self.direction.x * SPEED_OF_LIGHT / SLOWDOWN;
+        self.y += self.direction.y * SPEED_OF_LIGHT / SLOWDOWN;
+        self.push_back();
+    }
+}
+
+#[macroquad::main(window_config)]
+async fn main() {
+    let mut gargantua = BlackHole {
+        position: Vec2 {
+            x: 500.,
+            y: (HEIGHT / 2) as f32,
+        },
+        mass: 6e28,
+        r_s: 0.,
     };
 
-    let _win = sdl
-        .create_gl_window(win_args)
-        .expect("Failed to create a window");
+    let mut rays: Vec<Ray> = vec![Ray {
+        x: 100.,
+        y: 10. + ((HEIGHT / 2) as f32),
+        direction: Vec2 { x: 1., y: 0. },
+        trail: vec![],
+    }];
+    // get the schwarzschild radius of the black hole
+    gargantua.calc_r_s();
+    println!("{}", gargantua.r_s);
 
-    'main_window: loop {
-        while let Some(event) = sdl.poll_events() {
-            match event {
-                (events::Event::Quit, _) => break 'main_window,
-                _ => {}
-            }
+    // main app loop
+    loop {
+        clear_background(BLACK);
+        gargantua.draw();
+        for ray in &mut rays {
+            ray.draw();
+            ray.step();
+            ray.push_back();
+            // println!("{:?}", ray.trail);
         }
+        next_frame().await
     }
 }
